511b5d0d0ad0535f308e5ceca13e5cc88145810d
Kenneth Ormandy <kenneth@chloi.io>
2014-07-05 16:22:05 -0700

    Improves info being sent to GA & Segmentio

--- src/smokeproofs.js
+++ src/smokeproofs.js
@@ -10,7 +10,6 @@
  * Available under the MIT license
  */
 
-
 (function() {
 
   'use strict';
@@ -23,7 +22,7 @@
 
     SmokeProofs.prototype = {
 
-      _categorise: function(initiator) {
+      _label: function(initiator) {
         if(initiator === 'css') {
           return 'woff';
         } else if (initiator === 'link') {
@@ -39,7 +38,7 @@
         if(name.indexOf('themes.googleusercontent.com') !== -1 || name.indexOf('fonts.googleapis.com') !== -1) {
           return 'Google Fonts';
         } else if(name.indexOf('cloud.typography.com') !== -1) {
-          return 'H&FJ / H&Co. cloud.Typography';
+          return 'H&FJ / H&Co. Cloud.typography';
         } else if(name.indexOf('fast.fonts.net') !== -1) {
           return 'Monotype Fonts.com';
         } else if(name.indexOf('easy.myfonts.net') !== -1) {
@@ -49,58 +48,69 @@
         } else if(name.indexOf('fonts.typotheque.com') !== -1) {
           return 'Typotheque';
         } else {
-          return name;
+          return false;
         }
       },
 
       _init: function() {
         var resources = ''
+          , resourceService = false
+          , resourceFiletype = 'unknown'
           , i;
 
         if(typeof analytics === 'undefined' && typeof ga === 'undefined') {
           return console.warn('Analytics.js or Google Analytics are ' +
-                               'missing or not working properly. ' +
-                               'Need help? Open an issue on GitHub: ' +
-                               'https://github.com/kennethormandy/smokeproofs/issues');
+                              'missing or not working properly. ' +
+                              'Need help? Open an issue on GitHub: ' +
+                              'https://github.com/kennethormandy/smokeproofs/issues');
         } else {
           if(typeof window == 'object' && typeof window.performance == 'object') {
             if(typeof window.performance.getEntriesByName == 'function') {
               resources = window.performance.getEntriesByType('resource');
               for (i in resources) {
-                this._record(this._categorise(resources[i].initiatorType), this._parseName(resources[i].name), resources[i]);
+                resourceService = this._parseName(resources[i].name);
+                resourceFiletype = this._label(resources[i].initiatorType);
+                if(resourceService) {
+                  this._record(resourceService, resourceFiletype, resources[i]);
+                }
               }
             }
           }
         }
       },
 
-      _record: function(category, label, resource) {
+      _record: function(service, filetype, resource) {
+
         var dnsTime = Math.round(resource.domainLookupEnd - resource.domainLookupStart)
           , tcpTime = Math.round(resource.connectEnd - resource.connectStart)
           , totalTime = Math.round(resource.responseEnd - resource.startTime);
 
-        if(typeof analytics !== 'undefined') {
-          analytics.track('Loaded webfont ' + category, {
-            service: label,
+        if(typeof window.analytics !== 'undefined') {
+
+          window.analytics.track(service, {
+            service: service,
+            label: 'Load ' + filetype,
             dns: dnsTime,
             tcp: tcpTime,
             total: totalTime
           });
+
         } else if(typeof ga !== 'undefined') {
-          ga('send', 'timing', 'Loaded webfont ' + category, label, dnsTime, 'dns');
-          ga('send', 'timing', 'Loaded webfont ' + category, label, tcpTime, 'tcp');
-          ga('send', 'timing', 'Loaded webfont ' + category, label, totalTime, 'total');
+
+          // GA Timing Example
+          // ga('send', 'timing', 'timingCategory', 'timingVar',    timingValue, 'timingLabel');
+          // ga('send', 'timing', 'jQuery',         'Load Library', 20,          'Google CDN');
+
+          ga('send', 'timing', service, 'Load ' + filetype, dnsTime, 'DNS');
+          ga('send', 'timing', service, 'Load ' + filetype, tcpTime, 'TCP');
+          ga('send', 'timing', service, 'Load ' + filetype, totalTime, 'total');
+
         }
       }
 
     }
 
     return new SmokeProofs();
-    // return {
-    //   showConfig: function(){
-    //     return console.log("your config", config)
-    //   }
-    // }
   };
 
   if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
